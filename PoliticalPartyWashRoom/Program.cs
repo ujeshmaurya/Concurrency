bcdbb89ff391d0de516d2d24fe5c42f0b696d317
Ujesh Maurya <ujeshmourya@gmail.com>
2023-08-10 23:27:42 +0530

    Political Party WashRoom Concurrency Problem
    To run, use: dotnet run

---
+++ PoliticalPartyWashRoom/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PoliticalPartyWashRoom;
+
+class PoliticalPartyWashRoom {
+    /// DS for signifying the toilet stalls being used: Shared resource.
+    private static Queue<string> toiletStalls = new Queue<string>();
+
+    /// Lock for accessing the Shared resource.
+    private static object _lock = new object();
+
+    /// Entry point of dotnet program: Use 'dotnet run'
+    static void Main(string[] args) {
+        // Configure the various conditions here:
+
+        // Spawn threads here:
+    }    
+}
